6484580be685808adf1e8f2c830ca004e1124d23
cadaves <carole.daves@ni.com>
2014-09-17 13:42:47 -0500

    Updated 17Sep14

--- cachematrix.R
+++ cachematrix.R
@@ -1,14 +1,14 @@
 ## Creates a special "matrix" object that can cache its inverse
 
 makeCacheMatrix <- function(x = matrix()) {
-        mx <- NULL                         ## initializes matrix object
+        mx <- NULL                         ## initializes matrix object, local to current environment
         set <- function(y) {
                 x <<- y
                 mx <<- NULL
         }
         
         get <- function() {
-                x                  ## Returns the value of matrix x
+                x                       ## Returns the matrix x
         }
         
         setinverse <- function(inverse) {           
@@ -29,22 +29,24 @@ makeCacheMatrix <- function(x = matrix()) {
 ## The function cacheSolve checks to see if the inverse of the matrix has
 ## already been calculated.  If it has, it will access the inverse
 ## from the cache and print it.  If it has not, it will solve for
-## the inverse and then print it. Note that this assignment assumed that the
-## matrix would be a square one, and, thus, possible to calcualte the inverse.
-## If the matrix is singular (determinant =0), then the solve function will
-## return an error message.
+## the inverse and then print it. 
 
 cacheSolve <- function(x, ...) {
         mx <- x$getinverse()
         if(!is.null(mx)) {                       ##inverse already calculated?
-                message("getting cached data")   ##if yes, then gets cached inverse
+                message("getting cached data")   ##if yes, then prints msg & gets cached inverse
                 return(mx)
         }
-        data <- x$get()
-        mx <- solve(data, ...)          ## Else, calculates inverse. For this
-                                        ## assignment, assuming a square mx
+        data <- x$get()                         ## Else, calculates inverse
+        
+        if (nrow(data)==ncol(data)) {          ## Verifies matrix is square
+                if (det(data) != 0)  {          ## verifies mx nonsingular
+        mx <- solve(data, ...)          
+                                       
         x$setinverse(mx)
                 mx
+                } else {message("determinant =0")}
+        } else { message("matrix not square")}   ## if matrix not square, 
+                                                 ## then prints message
         
-        ## Return a matrix that is the inverse of 'x'
 }
